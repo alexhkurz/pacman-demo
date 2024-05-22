961fe0b8909468f3e658c877b7437cfd786014f1
Alexander Kurz <alexhkurz@gmail.com>
2024-05-21 22:04:16 -0700

    aider: Added scoring system and point object to Pacman game.
    # Aider chat conversation:
    
    USER: how do I start the app?
    
    ASSISTANT: To start the app, you don't need to modify any code. You just need to open the `index.html` file in a web browser. Here are the steps:
    
    1. Locate the `index.html` file in your project directory.
    2. Right-click on the file and select 'Open with'.
    3. Choose a web browser from the list of applications.
    
    The game should now load in your web browser. Use the arrow keys to move the Pacman character around the game board.
    
    USER: ok, cool. in the original pacman one can collect points by eating certain things. how could we add that?
    
    ASSISTANT: To add points to the game, we can create a point object on the game board that Pacman can "eat" to increase a score. We'll need to modify all three files: `index.html`, `styles.css`, and `pacman.js`.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    index.html
    ```html
    <<<<<<< SEARCH
    <body>
        <div id="game-board"></div>
        <script src="pacman.js"></script>
    </body>
    =======
    <body>
        <div id="score">Score: 0</div>
        <div id="game-board"></div>
        <script src="pacman.js"></script>
    </body>
    >>>>>>> REPLACE
    ```
    
    styles.css
    ```css
    <<<<<<< SEARCH
    .pacman {
        position: absolute;
        height: 20px;
        width: 20px;
        background-color: yellow;
    }
    =======
    .pacman {
        position: absolute;
        height: 20px;
        width: 20px;
        background-color: yellow;
    }
    
    .point {
        position: absolute;
        height: 10px;
        width: 10px;
        background-color: red;
    }
    >>>>>>> REPLACE
    ```
    
    pacman.js
    ```javascript
    <<<<<<< SEARCH
    let pacman = {
        top: 50,
        left: 50
    };
    =======
    let pacman = {
        top: 50,
        left: 50
    };
    
    let point = {
        top: 100,
        left: 100
    };
    
    let score = 0;
    >>>>>>> REPLACE
    ```
    
    pacman.js
    ```javascript
    <<<<<<< SEARCH
    document.getElementById('game-board').innerHTML = `<div class='pacman' style='top:${pacman.top}px;left:${pacman.left}px'></div>`;
    =======
    document.getElementById('game-board').innerHTML = `<div class='pacman' style='top:${pacman.top}px;left:${pacman.left}px'></div><div class='point' style='top:${point.top}px;left:${point.left}px'></div>`;
    >>>>>>> REPLACE
    ```
    
    pacman.js
    ```javascript
    <<<<<<< SEARCH
    document.onkeydown = function(e) {
        if (e.keyCode === 37) { // left arrow
            pacman.left -= 10;
        } else if (e.keyCode === 38) { // up arrow
            pacman.top -= 10;
        } else if (e.keyCode === 39) { // right arrow
            pacman.left += 10;
        } else if (e.keyCode === 40) { // down arrow
            pacman.top += 10;
        }
    
        document.getElementById('game-board').innerHTML = `<div class='pacman' style='top:${pacman.top}px;left:${pacman.left}px'></div>`;
    }
    =======
    document.onkeydown = function(e) {
        if (e.keyCode === 37) { // left arrow
            pacman.left -= 10;
        } else if (e.keyCode === 38) { // up arrow
            pacman.top -= 10;
        } else if (e.keyCode === 39) { // right arrow
            pacman.left += 10;
        } else if (e.keyCode === 40) { // down arrow
            pacman.top += 10;
        }
    
        if (pacman.top === point.top && pacman.left === point.left) {
            score += 1;
            document.getElementById('score').innerText = "Score: " + score;
            point.top = Math.floor(Math.random() * 280);
            point.left = Math.floor(Math.random() * 280);
        }
    
        document.getElementById('game-board').innerHTML = `<div class='pacman' style='top:${pacman.top}px;left:${pacman.left}px'></div><div class='point' style='top:${point.top}px;left:${point.left}px'></div>`;
    }
    >>>>>>> REPLACE
    ```
    
    This will add a red point to the game board that moves to a random location each time Pacman "eats" it, and a score that increases each time Pacman "eats" a point.

--- pacman.js
+++ pacman.js
@@ -3,6 +3,13 @@ let pacman = {
     left: 50
 };
 
+let point = {
+    top: 100,
+    left: 100
+};
+
+let score = 0;
+
 document.onkeydown = function(e) {
     if (e.keyCode === 37) { // left arrow
         pacman.left -= 10;
@@ -14,7 +21,14 @@ document.onkeydown = function(e) {
         pacman.top += 10;
     }
 
-    document.getElementById('game-board').innerHTML = `<div class='pacman' style='top:${pacman.top}px;left:${pacman.left}px'></div>`;
+    if (pacman.top === point.top && pacman.left === point.left) {
+        score += 1;
+        document.getElementById('score').innerText = "Score: " + score;
+        point.top = Math.floor(Math.random() * 280);
+        point.left = Math.floor(Math.random() * 280);
+    }
+
+    document.getElementById('game-board').innerHTML = `<div class='pacman' style='top:${pacman.top}px;left:${pacman.left}px'></div><div class='point' style='top:${point.top}px;left:${point.left}px'></div>`;
 }
 
-document.getElementById('game-board').innerHTML = `<div class='pacman' style='top:${pacman.top}px;left:${pacman.left}px'></div>`;
+document.getElementById('game-board').innerHTML = `<div class='pacman' style='top:${pacman.top}px;left:${pacman.left}px'></div><div class='point' style='top:${point.top}px;left:${point.left}px'></div>`;
